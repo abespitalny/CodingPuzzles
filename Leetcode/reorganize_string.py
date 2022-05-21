9e59ba1a4cbb990b5b879d5760dd575cc64f6637
Abraham <abespitalny@gmail.com>
2022-05-21 19:55:47 -0400

    Reorganize String

--- Leetcode/reorganize_string.py
+++ Leetcode/reorganize_string.py
@@ -8,6 +8,8 @@ from leetcode import *
 class Solution:
     # Time: O(n) Get counts in O(n), heapify, pop, and push into a heap of constant size given all inputs is constant time.
     # Space: O(1) excluding the output the heap and hashtable are limited by a constant which is the number of letters in the alphabet.
+    # My solution is a little different than others because I take the most common element and keep using it while getting the most common next element.
+    # Others are just using the two most common elements at every given stage, so there's only one while loop for the heap and not a nested pair.
     def reorganizeString(self, s: str) -> str:
         if len(s) <= 1:
             return s
