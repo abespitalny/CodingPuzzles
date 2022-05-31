cfe258841827ed0637ad09e1f7a47897e2ace054
Abraham <abespitalny@gmail.com>
2022-05-31 15:07:33 -0400

    Perfect Squares BFS

--- Leetcode/perfect_squares.py
+++ Leetcode/perfect_squares.py
@@ -34,10 +34,46 @@ class Solution:
             dp[i - 1] = minSquares
         return dp[n - 1]
 
+    # BFS approach
+    # Time: O(n**(h/2)) where h is the height of the tree.
+    # Space: O(n**(h/2)).
+    def numSquares2(self, n: int) -> int:
+        perfectSquares = [1]
+        while perfectSquares[-1] <= n:
+            nextSquare = len(perfectSquares) + 1
+            perfectSquares.append(nextSquare * nextSquare)
+
+        numSquares = 0
+        visited = set([n])
+        queue = deque([n])
+        while len(queue) != 0:
+            length = len(queue)
+            for i in range(length):
+                num = queue.popleft()
+                if num == 0:
+                    return numSquares
+
+                i = 0
+                while perfectSquares[i] <= num:
+                    nextNum = num - perfectSquares[i]
+                    if nextNum in visited:
+                        i += 1
+                        continue
+                    
+                    visited.add(nextNum)
+                    queue.append(nextNum)
+                    i += 1
+        
+            numSquares += 1
+
+        return -1
+
 solution = Solution()
 
 # Expected: 3
 print(solution.numSquares(12))
+print(solution.numSquares2(12))
 
 # Expected: 2
 print(solution.numSquares(13))
+print(solution.numSquares2(13))
