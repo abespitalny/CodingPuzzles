80ecf7420645c2822ed6ee0b065d2110d332ef36
Abraham <abespitalny@gmail.com>
2022-06-01 22:02:56 -0400

    Reverse String Recursion

--- Leetcode/reverse_string.py
+++ Leetcode/reverse_string.py
@@ -6,10 +6,9 @@ from leetcode import *
 # Time: O(n).
 # Space: O(1).
 class Solution:
+    # Time: O(n)
+    # Space: O(1)
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         halfLen = len(s) // 2
         i = 0
         while i < halfLen:
@@ -17,6 +16,19 @@ class Solution:
             s[i] = s[len(s) - 1 - i]
             s[len(s) - 1 - i] = temp
             i += 1
+
+    # Recursion approach
+    # Time: O(n)
+    # Space: O(n)
+    def reverseString2(self, s: List[str]) -> None:
+        def helper(s, start, end):
+            if (end - start) < 1:
+                return
+            s[start], s[end] = s[end], s[start]
+            helper(s, start + 1, end - 1)
+
+        helper(s, 0, len(s) - 1)
+
 
 solution = Solution()
 
