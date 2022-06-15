27972d552f470acec94bb70b86d0b024d1015b72
Abraham <abespitalny@gmail.com>
2022-06-15 14:20:59 -0400

    Leetcode's version

--- Leetcode/search_in_rotated_sorted_array.py
+++ Leetcode/search_in_rotated_sorted_array.py
@@ -71,6 +71,26 @@ def search2(nums: List[int], target: int) -> int:
 
     return -1
 
+# This was Leetcode's version which is a more readable version of mine above:
+    # start = 0
+    # end = len(nums) - 1
+    # while start <= end:
+    #     mid = (start + end) // 2
+    #     if nums[mid] == target:
+    #         return mid
+    #     elif nums[mid] >= nums[start]:
+    #         if target >= nums[start] and target < nums[mid]:
+    #             end = mid - 1
+    #         else:
+    #             start = mid + 1
+    #     else:
+    #         if target <= nums[end] and target > nums[mid]:
+    #             start = mid + 1
+    #         else:
+    #             end = mid - 1
+    # return -1
+
+
 print(search([4, 5, 6, 7, 0, 1, 2], 0))
 print(search2([4, 5, 6, 7, 0, 1, 2], 0))
 
