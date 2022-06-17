206f4f71fa9531ea8e58d74851c47c63e0ac13f4
Abraham <abespitalny@gmail.com>
2022-06-17 19:02:20 -0400

    Find the Duplicate Number - implement better approaches

--- Leetcode/find_the_duplicate_number.py
+++ Leetcode/find_the_duplicate_number.py
@@ -8,14 +8,66 @@ Follow up:
 '''
 from leetcode import *
 
-# Time: O(n), Space: O(n). Solution didn't satisfy problem constraints.
 class Solution:
+    # Time: O(n), Space: O(n). Solution didn't satisfy problem constraints.
     def findDuplicate(self, nums: List[int]) -> int:
         hashtable = set()
         for i in range(len(nums)):
             if nums[i] in hashtable:
                 return nums[i]
             hashtable.add(nums[i])
+
+    # This is the most ingenious solution!
+    # Turn the array into a linked list where the number at that position points to nums[nums[i]].
+    # There will be a cycle and the start of the cycle is the duplicate number,
+    # so we can use a modified solution to Linked List Cycle II to find the start of the cycle.
+    # Time: O(n)
+    # Space: O(1)
+    def findDuplicate2(self, nums: List[int]) -> int:
+        # Detect cycle using tortoise and hare.
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        # Find beginning of cycle which is the repeated number.
+        start = nums[0]
+        while start != fast:
+            start = nums[start]
+            fast = nums[fast]
+
+        return start
+
+    # Binary search approach
+    # Time: O(n*log(n))
+    # Space: O(1)
+    def findDuplicate3(self, nums: List[int]) -> int:
+        def countLessThanNum(nums, num):
+            count = 0
+            for i in nums:
+                if i <= num:
+                    count += 1
+            return count
+
+        left = 1
+        right = len(nums)
+        while (left + 1) < right:
+            mid = (left + right) // 2
+
+            countNums = countLessThanNum(nums, mid)
+            
+            if countNums <= mid:
+                left = mid
+            else:
+                right = mid
+
+        if countLessThanNum(nums, left) > left:
+            return left
+        return right
 
 # Had to look at solutions on leetcode.
 # There are several good approaches.
@@ -26,3 +78,16 @@ class Solution:
 # 2) Count the number of ones in each bit position for nums and for 1..n then subtract the two. This'll give you the duplicate. Runs in O(n*log(n)) with O(1) space.
 # 3) This is the most ingenious solution! Turn the array into a linked list where the number at that position points to nums[nums[i]]. There will be a cycle and the
 #    start of the cycle is the duplicate number, so we can use a modified solution to Linked List Cycle II to find the start of the cycle. Can be done in O(n) time and O(1) space.
+
+solution = Solution()
+
+assert solution.findDuplicate([1,3,4,2,2]) == 2
+assert solution.findDuplicate2([1,3,4,2,2]) == 2
+assert solution.findDuplicate3([1,3,4,2,2]) == 2
+
+assert solution.findDuplicate([3,1,3,4,2]) == 3
+assert solution.findDuplicate2([3,1,3,4,2]) == 3
+assert solution.findDuplicate3([3,1,3,4,2]) == 3
+
+assert solution.findDuplicate3([1, 1]) == 1
+
